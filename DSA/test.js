edbdb73dc3aa8c971f80014f042e8d64b2c2b32d
Jay Patil <122766420+JayP45@users.noreply.github.com>
2024-01-06 15:48:24 +0530

    updates

--- DSA/test.js
+++ DSA/test.js
@@ -1,42 +1,42 @@
-//01 Reverse array and return new array
+// //01 Reverse array and return new array
 
-let reverseArray = () => {
-    let array = [1, 2, 3, 4, 5]
-    // let newArray = array.map((x,y)=> x.length-1) 
-    // console.log(newArray)
+// let reverseArray = () => {
+//     let array = [1, 2, 3, 4, 5]
+//     // let newArray = array.map((x,y)=> x.length-1) 
+//     // console.log(newArray)
 
-    for (let i = array.length; i >= 0; i--) {
-        console.log(array[i])
-    }
+//     for (let i = array.length; i >= 0; i--) {
+//         console.log(array[i])
+//     }
 
-}
-reverseArray()
+// }
+// reverseArray()
 
-//02 Remove Duplicates
+// //02 Remove Duplicates
 
-let a = [1,2,3,2,2,3,4,4,5]
-let unique = new Set(a)
-console.log([...unique])
+// let a = [1,2,3,2,2,3,4,4,5]
+// let unique = new Set(a)
+// console.log([...unique])
 
-//03 Sort array ascending
+// //03 Sort array ascending
 
-let sortArray = [5,4,3,2,1]
-sortArray.sort()
-//  let ascendingArray=(s)=>{
-//      s.sort()
-//  }
-console.log(sortArray)
+// let sortArray = [5,4,3,2,1]
+// sortArray.sort()
+// //  let ascendingArray=(s)=>{
+// //      s.sort()
+// //  }
+// console.log(sortArray)
 
-//04 calculate sum of array
+// //04 calculate sum of array
 
-let calculateArray=()=>{
-    let arr = [1,2,3,4,5]
-    let cal = arr.reduce((x,y)=>{
-        return x + y
-    })
-    console.log(cal)
-}
-calculateArray()
+// let calculateArray=()=>{
+//     let arr = [1,2,3,4,5]
+//     let cal = arr.reduce((x,y)=>{
+//         return x + y
+//     })
+//     console.log(cal)
+// }
+// calculateArray()
 
 //05 duplicate string
 
